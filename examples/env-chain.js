0f52d1f7df56fc39fec47d122da21d4c4630b9c9
ran <abbshrsoufii@gmail.com>
2018-07-27 17:14:36 +0800

    流计算任务: env 对象, op, 管道资源管理; 任务存取器

---
+++ examples/env-chain.js
@@ -0,0 +1,77 @@
+(async () => {
+  const { Env, operators } = await require("..")({ task_store: { cfg: { schema: "client:" } } });
+  try {
+    const env = new Env(null);
+
+    const src_1 = env.from(new operators.GeneratorSource(null, {
+      emitter() { return Math.random() * 1000 >> 0; },
+      frequency: 2000,
+      limit: 5,
+    }));
+
+    const src_2 = env.from(new operators.GeneratorSource(null, {
+      frequency: 1000,
+      limit: 20,
+    }));
+
+    const calc = src_1
+      .tap(elem => console.log("Tap", elem));
+    src_2.pipe(calc);
+
+    await Promise.all([
+      calc.to(new operators.ValidateSink(null, elem => {
+        console.log("CHECK_1:", elem);
+        return elem.record > 600 || elem.record <= 1;
+      })),
+
+      calc.to(new operators.ValidateSink(null, elem => {
+        console.log("CHECK_2:", elem);
+        return elem.record < 1000;
+      }))
+    ]);
+
+    console.log("R", "DONE");
+  } catch (err) {
+    console.log("E", err);
+  }
+})();
+// const GeneratorSource = require("../lib/sources/generator");
+// const BlankCalculator = require("../lib/calculators");
+// const ValidateSink = require("../lib/sinks/validate");
+// const pEvent = require("p-event");
+
+// const src = new GeneratorSource(null, {
+//   emitter() { return Math.random() * 1000 >> 0; },
+//   frequency: 2000,
+//   limit: 5,
+// });
+
+// const src_2 = new GeneratorSource(null, {
+//   frequency: 1000,
+//   limit: 20,
+// });
+
+// const cal = new BlankCalculator();
+// const dest = new ValidateSink(null, elem => {
+//   console.log("CHECK_1:", elem);
+//   return elem.record > 200 || elem.record <= 1;
+// });
+
+// const dest_2 = new ValidateSink(null, elem => {
+//   console.log("CHECK_2:", elem);
+//   return elem.record < 1000;
+// });
+
+// Object.assign(global, {src, cal, dest});
+
+//      dest_2
+//         ^
+//         |
+// src -> cal -> dest
+//         ^
+//         |
+//       src2
+// src.pipe(cal, {end: false}).pipe(dest, {end: false});
+// src_2.pipe(cal, {end: false}).pipe(dest_2, {end: false});
+
+// pEvent(dest, "operator_done", { rejectionEvents: ["operator_error"] }).then(() => console.log("DONE")).catch(err => console.log("ERR", err));
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-// debug only
+
+// TODO: a better way
 if (!global.repl) {
   const timers = require("safe-timers");
   global.setTimeout = timers.setTimeout;
   global.clearTimeout = timers.clearTimeout;
 } else {
+  // debug only
   console.log("WARN: REPL环境使用原生 timer");
 }
 
@@ -14,7 +16,37 @@ const Mass = {
   MassBus: require("./lib/bus"),
   Env: require("./lib/env"),
   operators: require("./lib/operators").loadall(),
-  // MassDataSet: require("./job/data")
+  // MassDataSet: require("./job/data"),
+  ready: false,
+};
+
+async function initExternalResources(cfg) {
+  await initRedis(cfg.task_store);
 }
 
-module.exports = Mass
+async function initRedis(cfg) {
+  try {
+    await require("./lib/redis").connect(cfg);
+    console.log("初始化外部资源: Redis,", "配置:", cfg);
+  } catch (err) {
+    err.resourceName = "Redis";
+    err.cfg = cfg;
+    throw err;
+  }
+}
+
+module.exports = async (cfg) => {
+  // 统一初始化外部依赖资源
+  if (!Mass.ready) {
+    try {
+      await initExternalResources(cfg);
+      Mass.ready = true;
+    } catch (err) {
+      console.log(`外部资源 ${err.resourceName} 初始化失败:`, err.message);
+      console.log("配置:", err.cfg);
+      console.log("stack:", err.stack);
+    }
+  }
+
+  return Mass;
+};
