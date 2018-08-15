f47302f2b732a6bf0e7428860c1fc3a387960606
ran <abbshrsoufii@gmail.com>
2018-08-15 16:20:33 +0800

    完善子任务容错

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const mass = {
   MassTask: require("./lib/task"),
   MassStreamTask: require("./lib/stream-task"),
   MassTaskScheduler: require("./lib/scheduler"),
+  strategies: require("./lib/fault-tolerate"),
 
   stream: {
     Env: require("./lib/env"),
