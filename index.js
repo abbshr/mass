aa3a3bc83b5085256d02d2619577f32248f5bbff
ran <abbshrsoufii@gmail.com>
2018-09-14 13:00:05 +0800

    全局使用新的 Log 模块

--- index.js
+++ index.js
@@ -9,9 +9,37 @@ const mass = {
     Gen: require("./lib/gen"),
     operators: require("./lib/operators"),
     states: require("./lib/states"),
+
+    // 设置所有运算符的默认失败处理模式
+    set fault_mode(mode) {
+      this.operators.Source.fault_mode = mode;
+      this.operators.Calculator.fault_mode = mode;
+      this.operators.Sink.fault_mode = mode;
+    },
+
+    fault: {
+      mode: {
+        IGNORE: "ignore",
+        THROW: "throw",
+      },
+    },
   },
 
+  // 配置
   config: require("./lib/config"),
+
+  // 应用日志
+  get applog() {
+    return require("./lib/applog")();
+  },
+
+  get log() {
+    return this.applog;
+  },
+
+  get syslog() {
+    return require("./lib/syslog")();
+  },
 };
 
 module.exports = mass;
